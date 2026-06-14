7b9af8742c4b9ac18165a81eb312d7e4134d151e
Cesar Romero <cesarromero34@gmail.com>
2026-06-14 15:38:36 -0300

    feat: implementa novo retorno do formatador de datas

--- src/data-formatter.ts
+++ src/data-formatter.ts
@@ -2,13 +2,18 @@ export function zeroAEsquerda(num: number){
     return num >= 10 ? num : `0${num}`
 }
 
-export function formatDate(date: Date): string{
+export function formatDate(date: Date, showDateAndTime: boolean = false): string{
     const dia = zeroAEsquerda(date.getDate())
     const mes = zeroAEsquerda(date.getMonth() + 1)
     const ano = zeroAEsquerda(date.getFullYear())
-    // const hora = zeroAEsquerda(date.getHours())
-    // const min = zeroAEsquerda(date.getMinutes())
-    // const sec = zeroAEsquerda(date.getSeconds())
-
+    const hora = zeroAEsquerda(date.getHours())
+    const min = zeroAEsquerda(date.getMinutes())
+    const sec = zeroAEsquerda(date.getSeconds())
+    if(showDateAndTime){
+        return `${dia}/${mes}/${ano} ${hora}:${min}:${sec}`
+    }
+    else
+    {
     return ` ${dia}/${mes}/${ano}`
+    }
 }

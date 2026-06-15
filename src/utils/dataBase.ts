fb424448432e0c48c3c002ab809953b53b9eac41
Cesar Romero <cesarromero34@gmail.com>
2026-06-14 22:41:57 -0300

    feat: implementa a conexão com o banco de dados

---
+++ src/utils/dataBase.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import LoggerHelper from "../utils/logger";
+import { ENVIRONMENT } from "../environment";
+
+export async function connectDB(logger: LoggerHelper): Promise<void> {
+  if (!ENVIRONMENT.mongoose.uri) {
+    logger.log("ERROR", "MONGO_URI não definido no .env");
+    process.exit(1);
+  }
+
+  try {
+    mongoose.connection.on("error", (err) =>
+      console.error("Mongoose error:", err),
+    );
+    await mongoose.connect(ENVIRONMENT.mongoose.uri!, {
+      serverSelectionTimeoutMS: 10000,
+    });
+    logger.log("INFO", "MongoDB conectado.");
+  } catch (err: any) {
+    console.log("Erro ao conectar ao banco de dados: " + err.message);
+    process.exit(1);
+  }
+}

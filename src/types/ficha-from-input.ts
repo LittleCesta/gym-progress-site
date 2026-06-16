f76254055f7c8dbef637ff6868d4c75461d8e878
Cesar <cesarromero34@gmail.com>
2026-06-16 17:51:55 -0300

    feat: implementa type da ficah recebida do front

---
+++ src/types/ficha-from-input.ts
@@ -0,0 +1,14 @@
+export type FichaInputData = {
+  nome: string;
+  email?: string;
+  peso: number;
+  peito: number;
+  abdomen: number;
+  ombros: number;
+  quadricepsEsquerdo: number;
+  quadricepsDireito: number;
+  panturrilhaEsquerda: number;
+  panturrilhaDireita: number;
+  bicepsEsquerdo: number;
+  bicepsDireito: number;
+};

e6ee25e49759463681db244c273bf8928a96569a
Cesar <cesarromero34@gmail.com>
2026-06-16 17:52:33 -0300

    chore: ajusta a inicialização do servidor

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ process.env.NODE_ENVIRONMENT == "development"
   ? process.loadEnvFile(".env.development")
   : process.loadEnvFile(".env.production");
 
-import "./server";
 import { startServer } from "./server";
 import Logger from "./utils/logger";
 
 (async function () {
   const logger = new Logger("./gym-progress.log", "gym-progress");
 
-  await startServer();
+  await startServer(logger);
 })();

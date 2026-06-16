cfd011ed5efdd8d1fd61ad2bb25950d690374608
Cesar <cesarromero34@gmail.com>
2026-06-16 17:50:23 -0300

    feat: implementa validação para buscar por nome ou buscar todas as fichas e implementa a busca utilizando o Enter

--- src/front/main.ts
+++ src/front/main.ts
@@ -1,6 +1,6 @@
 import { toggleMenu } from "./toggle-menu";
 import { initCadastroFicha } from "./cadastro-fichas";
-import { initListarFichas } from "./get-fichas";
+import { buscarTodasAsFichas, buscarFichaEspecifica } from "./get-fichas";
 
 document.addEventListener("DOMContentLoaded", () => {
   toggleMenu();
@@ -12,6 +12,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const containerFichas: HTMLElement = document.querySelector(".lista-fichas");
   if (containerFichas) {
-    initListarFichas(containerFichas);
+    const searchFichasButton: HTMLButtonElement =
+      document.querySelector(".btn-search");
+    const searchFichasInput: HTMLInputElement =
+      document.querySelector("#search-bar");
+
+    searchFichasButton.addEventListener("click", () => {
+      const termo = searchFichasInput.value.trim();
+      if (termo !== "") {
+        buscarFichaEspecifica(containerFichas, termo);
+      } else {
+        buscarTodasAsFichas(containerFichas);
+      }
+    });
+
+    searchFichasButton.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        const termo = searchFichasInput.value.trim();
+        if (termo !== "") {
+          buscarFichaEspecifica(containerFichas, termo);
+        } else {
+          buscarTodasAsFichas(containerFichas);
+        }
+      }
+    });
   }
 });

9c8c41e1485dbb2c1dba7f5c699ad97925289f86
Cesar <cesarromero34@gmail.com>
2026-06-17 12:29:10 -0300

    feat: implementa método de exclusão da ficha por ID

---
+++ src/modules/fichas/fichas.controller.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import { FichaSchema } from "./fichas.schema";
+import { fichaService } from "./fichas.service";
+import LoggerHelper from "../../shared/utils/logger";
+
+export class FichaController {
+  constructor(private logger: LoggerHelper) {}
+
+  listarFichas = async (req: Request, res: Response) => {
+    try {
+      const fichas = await fichaService.getFichas(this.logger);
+      return res.json(fichas);
+    } catch (err) {
+      return res.status(500).json({ error: "Erro ao buscar fichas" });
+    }
+  };
+
+  salvarFicha = async (req: Request, res: Response) => {
+    try {
+      const resultado = FichaSchema.safeParse(req.body);
+      if (!resultado.success) {
+        return res.status(400).json({ error: resultado.error.issues });
+      }
+
+      const novaFicha = await fichaService.createFicha(
+        resultado.data,
+        this.logger,
+      );
+      if (!novaFicha) {
+        return res
+          .status(500)
+          .json({ error: "Erro interno ao salvar no banco." });
+      }
+
+      return res.status(201).json(novaFicha);
+    } catch (err: any) {
+      this.logger.log("ERROR", `Erro no cadastro: ${err.message}`);
+      return res.status(500).json({ error: "Erro ao processar a requisição." });
+    }
+  };
+
+  buscarPorNome = async (req: Request, res: Response) => {
+    try {
+      const ficha = await fichaService.buscarFichasPorNome(
+        String(req.params.nome),
+        this.logger,
+      );
+      return res.json(ficha);
+    } catch (err) {
+      return res.status(500).json({ error: "Erro ao buscar ficha" });
+    }
+  };
+
+  deletarFicha = async (req: Request, res: Response) => {
+    try {
+      const ficha = await fichaService.deletarFicha(
+        String(req.params._id),
+        this.logger,
+      );
+      return res.json(ficha);
+    } catch (err) {
+      return res.status(500).json({ error: "Erro ao deletar ficha" });
+    }
+  };
+}

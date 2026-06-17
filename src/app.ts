60e13dfca40aef478dbe34f033fb7122b70021ea
Cesar <cesarromero34@gmail.com>
2026-06-17 12:30:55 -0300

    chore: centraliza configurações do express

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+// src/app.ts
+import express from "express";
+import path from "path";
+import { createRouter } from "./routes";
+import LoggerHelper from "./shared/utils/logger";
+
+export function createApp(logger: LoggerHelper) {
+  const app = express();
+
+  // Middlewares Globais
+  app.use(express.json());
+
+  // Servir arquivos estáticos (CSS, JS, Imagens) mapeado corretamente para evitar erro MIME
+  app.use(
+    "/assets",
+    express.static(path.join(process.cwd(), "public", "assets")),
+  );
+
+  // Injeção de Rotas
+  app.use(createRouter(logger));
+
+  return app;
+}

bdc61ca5f3b3ddde296f506a1e155d0016c92f62
Cesar <cesarromero34@gmail.com>
2026-06-17 12:31:13 -0300

    chore: ajusta importação do helper de logger

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ process.env.NODE_ENVIRONMENT == "development"
   : process.loadEnvFile(".env.production");
 
 import { startServer } from "./server";
-import Logger from "./utils/logger";
+import Logger from "./shared/utils/logger";
 
 (async function () {
   const logger = new Logger("./gym-progress.log", "gym-progress");

3730b2a06369ff4be7d9be3f63b1ca2a07c8680a
Cesar <cesarromero34@gmail.com>
2026-06-19 16:33:15 -0300

    feat: atualiza endpoints para renderizar arquivos ejs e insere endpoints para usuarios

--- src/routes.ts
+++ src/routes.ts
@@ -1,33 +1,44 @@
 // src/routes.ts
 import { Router } from "express";
-import path from "path";
 import { FichaController } from "./controllers/FichaController";
 import LoggerHelper from "./shared/utils/logger";
+import { UsuarioController } from "./controllers/UsuarioController";
 
 export function createRouter(logger: LoggerHelper): Router {
   const router = Router();
+
   const fichaController = new FichaController(logger);
+  const usuarioController = new UsuarioController(logger);
 
   // --- HTML Views ---
-  const viewsPath = path.join(process.cwd(), "public", "assets");
-
   router.get("/", (req, res) =>
-    res.sendFile(path.join(viewsPath, "index.html")),
+    res.render("index", { titulo: "Menu principal" }),
   );
+  router.get("/login", (req, res) => res.render("login", { titulo: "Login" }));
   router.get("/fichas-salvas", (req, res) =>
-    res.sendFile(path.join(viewsPath, "fichas-salvas.html")),
+    res.render("fichas-salvas", { titulo: "Fichas de Treino" }),
   );
   router.get("/cadastro-de-fichas", (req, res) =>
-    res.sendFile(path.join(viewsPath, "cadastro-de-fichas.html")),
+    res.render("cadastro-de-fichas", { titulo: "Cadastro de Ficha" }),
   );
 
   // --- API Endpoints ---
+  // Fichas
   router.get("/api/listar-fichas", fichaController.listarFichas);
   router.post("/api/salvar-ficha", fichaController.salvarFicha);
   router.delete("/api/deletar-ficha/:_id", fichaController.deletarFicha);
   router.get(
     "/api/listar-ficha-especifica/:nome",
-    fichaController.buscarPorNome,
+    fichaController.buscarFichaPorNome,
+  );
+
+  // Usuários
+  router.get("/api/listar-usuarios", usuarioController.listarUsuarios);
+  router.post("/api/salvar-usuario", usuarioController.salvarUsuario);
+  router.delete("/api/deletar-usuario/:_id", usuarioController.deletarUsuario);
+  router.post(
+    "/api/listar-usuario-especifico/:nome",
+    usuarioController.buscarUsuarioPorNome,
   );
 
   return router;
